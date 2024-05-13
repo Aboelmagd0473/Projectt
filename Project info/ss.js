6df17d1fb649645818930b42167a15bf42c73882
Aboelmagd0473 <169081180+Aboelmagd0473@users.noreply.github.com>
2024-05-13 12:01:34 +0300

    Add files via upload

---
+++ Project info/ss.js
@@ -0,0 +1,9 @@
+let title = document.title;
+alert("مرحبا");
+
+document.body.onload = ()=>{
+alert("Downloaded")
+};
+prompt("Enter your name" ,"EX: Abdelrahman Ahmed");
+confirm("Are you sure to open this page");
+
